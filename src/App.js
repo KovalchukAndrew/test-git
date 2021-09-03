645d848e033e8fcac6ec0b7194a2dd63c2c30c11
KovalchukAndrew <dry.st.jim@gmail.com>
2021-09-03 14:35:18 +0300

    add new text

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    function yo (a, b) {
+        return a + b
+    }
   return (
     <div className="App">
       <p>Hello</p>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
     </div>
   );
 }
